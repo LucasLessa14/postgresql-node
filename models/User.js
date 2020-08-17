48e241015a5347bafaceaa2cf55509ac1f180a30
Lucas Lessa <lucaslessa14@hotmail.com>
2020-08-17 14:53:50 -0300

    One-to-one relationship

--- models/User.js
+++ models/User.js
@@ -7,6 +7,11 @@ class User extends Model {
             email: DataTypes.STRING,
         }, { sequelize });
     }
+
+    static associate(models) {
+        this.hasMany(models.Adress, { foreignKey: 'user_id', as: 'adresses' });
+    }
 }
+
 
 module.exports = User;
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
 const userController = require('../controllers/userController');
+const adressController = require('../controllers/adressController');
 
 routes.get('/users', userController.index);
 routes.post('/users', userController.store);
+
+routes.get('/users/:user_id/adresses', adressController.index);
+routes.post('/users/:user_id/adresses', adressController.store);
 
 module.exports = routes;
